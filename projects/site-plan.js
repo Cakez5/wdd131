388f2d6a7e45761abd3ed86589d213c0a7330040
Cakez5 <zmartinsen@byupathway.edu>
2025-11-29 17:15:21 -0700

    made some fixes

--- projects/site-plan.js
+++ projects/site-plan.js
@@ -1,3 +1,9 @@
-document.getElementById("currentyear").textContent = new Date().getFullYear();
-document.getElementById("lastModified").textContent =
-  "Last Modified: " + document.lastModified;
+// Format last modified date
+const options = { year: 'numeric', month: 'short', day: 'numeric' };
+const formattedDate = new Date(document.lastModified).toLocaleDateString(undefined, options);
+
+// Update year
+document.getElementById("year").textContent = new Date().getFullYear();
+
+// Update last modified
+document.getElementById("lastModified").textContent = "Last Modified: " + formattedDate;
